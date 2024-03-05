8cc0a61917135aa1d4745c96e9296b6bfbe7229f
unknown <sofiamaglyona@gmail.com>
2024-03-05 15:07:32 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <title><?= 'My Site!' ?></title>
+ 
+ <!-- Meta -->
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta name="description" content="">
+ <meta name="author" content="">
+ 
+ <!-- Global CSS -->
+ <link rel="stylesheet" href="assets/css/my-styles.css">
+</head>
+<body>
+
+<!-- ******HEADER****** -->
+
+<header id="header" class="header">
+
+<div class="nav-menu">
+ <ul class="menu"> 
+ <li class="link">Home Page</li>
+ <li class="link">About Us</li>
+ <li class="link">Contact Us</li>
+ </ul>
+</div>
+
+
+
+
+</header>
+
+<!-- main content -->
+<main class="main content">
+</main>
+
+<!-- ******FOOTER****** -->
+<footer class="footer">
+ <div class="container text-center">
+ <small class="copyright"><?= 'Designed by Me!' ?></small>
+ </div>
+ <!--//container-->
+
+</footer>
+<!--//footer-->
+<!-- Javascript -->
+</body>
+</html>
